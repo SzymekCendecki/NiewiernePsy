7c07200985c82892e5fdeedc433aff37a7637db7
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2020-08-23 21:30:09 +0200

    Add files via upload

--- js/engine.js
+++ js/engine.js
@@ -77,224 +77,24 @@ module.exports = __webpack_require__(1);
 "use strict";
 
 
-var _classes = __webpack_require__(2);
+document.querySelector("#aboutBtn").addEventListener("click", function () {});
 
-var header = new _classes.Element("header", "body");
-header.createElement();
+document.querySelector("#gamesBtn").addEventListener("click", function () {});
 
-var h1 = new _classes.Element("h1", "header");
-h1.createElement();
-document.querySelector("h1").innerHTML = "NIEWIERNE PSY";
+document.querySelector("#programsBtn").addEventListener("click", function () {});
 
-var h3 = new _classes.Element("h3", "header");
-h3.createElement();
-document.querySelector("h3").innerHTML = "nemo est dominus meus";
+document.querySelector("#streamBtn").addEventListener("click", function () {});
 
-var nav = new _classes.Element("nav", "body");
-nav.createElement();
+document.querySelector("#contactBtn").addEventListener("click", function () {});
 
-var home = new _classes.ElementIdTxt("div", "nav", "home", "START");
-home.createElement();
+fetch('./json/test.json').then(function (response) {
+    return response.json();
+}).then(function (data) {
 
-var about = new _classes.ElementIdTxt("div", "nav", "about", "O NAS");
-about.createElement();
-
-var games = new _classes.ElementIdTxt("div", "nav", "games", "GRY");
-games.createElement();
-
-var projects = new _classes.ElementIdTxt("div", "nav", "projects", "PROJEKTY");
-projects.createElement();
-
-var graphic = new _classes.ElementIdTxt("div", "nav", "graphic", "GRAFIK");
-graphic.createElement();
-
-var mainContainer = new _classes.ElementId("div", "body", "mainContainer");
-mainContainer.createElement();
-
-var path = 'https://szymekcendecki.github.io/NiewiernePsy/json/';
-
-document.querySelector("#home").addEventListener("click", function () {
-    fetch(path + 'home.json').then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        document.querySelector("#mainContainer").innerHTML = "";
-        document.querySelector("#mainContainer").innerHTML = data.txt1;
-    }).catch(function (error) {
-        return console.error(error);
-    });
+    console.log(data.test);
+}).catch(function (error) {
+    return console.error(error);
 });
-
-document.querySelector("#about").addEventListener("click", function () {
-    fetch(path + 'about.json').then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        document.querySelector("#mainContainer").innerHTML = "";
-        document.querySelector("#mainContainer").innerHTML = data.about;
-    }).catch(function (error) {
-        return console.error(error);
-    });
-});
-
-document.querySelector("#games").addEventListener("click", function () {
-    fetch(path + 'games.json').then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        document.querySelector("#mainContainer").innerHTML = "";
-
-        for (var i = 1; i < Object.values(data).length - 1; i++) {
-            var element = document.createElement('p');
-            element.innerHTML = "<a href=\"" + Object.values(data)[i] + "\" target=\"_blank\">" + Object.keys(data)[i] + "</a>";
-            document.querySelector('#mainContainer').appendChild(element);
-        }
-    }).catch(function (error) {
-        return console.error(error);
-    });
-});
-
-document.querySelector("#projects").addEventListener("click", function () {
-    fetch(path + 'projects.json').then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        document.querySelector("#mainContainer").innerHTML = "";
-
-        for (var i = 1; i < Object.values(data).length - 1; i++) {
-            var element = document.createElement('p');
-            element.innerHTML = "<a href=\"" + Object.values(data)[i] + "\" target=\"_blank\">" + Object.keys(data)[i] + "</a>";
-            document.querySelector('#mainContainer').appendChild(element);
-        }
-    }).catch(function (error) {
-        return console.error(error);
-    });
-});
-
-document.querySelector("#graphic").addEventListener("click", function () {
-    fetch(path + 'graphic.json').then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        document.querySelector("#mainContainer").innerHTML = "";
-
-        var p = document.createElement("p");
-        p.innerHTML = "Stream z 23 maja 2020 z Neverwinter Nights został usunięty. Ponoć zostały naruszone czyjeś prawa autorskie. Niewierne Psy nie maja pojęcia w jaki sposób to się mogło stać. Życie. W miejsce Neverwiter Nights będzie teraz Total War - Napoleon. Zobaczym jak to wyjdzie.";
-        document.querySelector('#mainContainer').appendChild(p);
-
-        var p2 = document.createElement("p");
-        p2.innerHTML = "Ważna informacja. Od jutra (26 czerwca 2020), do odwołania, będzie zmiana w grafiku oraz w grach. Streamowana będzie także tylko jedna gra: Eve Online. Zmianie ulegnie platforma, na której będą odbywały się streamy, a będzie to twitch.tv. W planach jest nowy grafik. 26 czerwca 2020 będzie przeprowadzany test na wymienionej platformie. Wstępnie streamy będą w piątki (19 - 22) oraz niedzielę (17 - 20). Czasami będzie stream także w soboty (19 - 22).";
-        document.querySelector('#mainContainer').appendChild(p2);
-    }).catch(function (error) {
-        return console.error(error);
-    });
-});
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Element = exports.Element = function () {
-    function Element(what, where) {
-        _classCallCheck(this, Element);
-
-        this.what = what;
-        this.where = where;
-    }
-
-    _createClass(Element, [{
-        key: "createElement",
-        value: function createElement() {
-            var el = document.createElement(this.what);
-            document.querySelector(this.where).append(el);
-        }
-    }]);
-
-    return Element;
-}();
-
-var ElementId = exports.ElementId = function (_Element) {
-    _inherits(ElementId, _Element);
-
-    function ElementId(what, where, id) {
-        _classCallCheck(this, ElementId);
-
-        var _this = _possibleConstructorReturn(this, (ElementId.__proto__ || Object.getPrototypeOf(ElementId)).call(this, what, where));
-
-        _this.id = id;
-        return _this;
-    }
-
-    _createClass(ElementId, [{
-        key: "createElement",
-        value: function createElement() {
-            var el = document.createElement(this.what);
-            el.id = this.id;
-            document.querySelector(this.where).append(el);
-        }
-    }]);
-
-    return ElementId;
-}(Element);
-
-var ElementTxt = exports.ElementTxt = function (_Element2) {
-    _inherits(ElementTxt, _Element2);
-
-    function ElementTxt(what, where, txt) {
-        _classCallCheck(this, ElementTxt);
-
-        var _this2 = _possibleConstructorReturn(this, (ElementTxt.__proto__ || Object.getPrototypeOf(ElementTxt)).call(this, what, where));
-
-        _this2.txt = txt;
-        return _this2;
-    }
-
-    _createClass(ElementTxt, [{
-        key: "createElement",
-        value: function createElement() {
-            var el = document.createElement(this.what);
-            el.append(this.text);
-            document.querySelector(this.where).append(el);
-        }
-    }]);
-
-    return ElementTxt;
-}(Element);
-
-var ElementIdTxt = exports.ElementIdTxt = function (_ElementId) {
-    _inherits(ElementIdTxt, _ElementId);
-
-    function ElementIdTxt(what, where, id, text) {
-        _classCallCheck(this, ElementIdTxt);
-
-        var _this3 = _possibleConstructorReturn(this, (ElementIdTxt.__proto__ || Object.getPrototypeOf(ElementIdTxt)).call(this, what, where, id));
-
-        _this3.text = text;
-        return _this3;
-    }
-
-    _createClass(ElementIdTxt, [{
-        key: "createElement",
-        value: function createElement() {
-            var el = document.createElement(this.what);
-            el.id = this.id;
-            el.innerHTML = this.text;
-            document.querySelector(this.where).append(el);
-        }
-    }]);
-
-    return ElementIdTxt;
-}(ElementId);
 
 /***/ })
 /******/ ]);
--- js/workingFile.js
+++ js/workingFile.js
@@ -1,93 +1,27 @@
-import { Element, ElementId, ElementTxt, ElementIdTxt } from './classes/classes.js';
 
-let header = new Element("header", "body");
-header.createElement();
+document.querySelector("#aboutBtn").addEventListener("click", ()=>{
 
-let h1 = new Element("h1", "header");
-h1.createElement();
-document.querySelector("h1").innerHTML = "NIEWIERNE PSY";
-
-let h3 = new Element("h3", "header");
-h3.createElement();
-document.querySelector("h3").innerHTML = "nemo est dominus meus";
-
-
-let nav = new Element("nav", "body");
-nav.createElement();
-
-let home = new ElementIdTxt("div", "nav", "home", "START");
-home.createElement();
-
-let about = new ElementIdTxt("div", "nav", "about", "O NAS");
-about.createElement();
-
-let games = new ElementIdTxt("div", "nav", "games", "GRY");
-games.createElement();
-
-let projects = new ElementIdTxt("div", "nav", "projects", "PROJEKTY");
-projects.createElement();
-
-let graphic = new ElementIdTxt("div", "nav", "graphic", "GRAFIK");
-graphic.createElement();
-
-let mainContainer = new ElementId("div", "body", "mainContainer");
-mainContainer.createElement();
-
-const path = 'https://szymekcendecki.github.io/NiewiernePsy/json/'
-
-document.querySelector("#home").addEventListener("click", ()=>{
-    fetch(path + 'home.json').then(response => response.json()).then(data => { 
-        document.querySelector("#mainContainer").innerHTML = "";
-        document.querySelector("#mainContainer").innerHTML = data.txt1;
-     }).catch(error => console.error(error))
 });
 
-document.querySelector("#about").addEventListener("click", ()=>{
-    fetch(path + 'about.json').then(response => response.json()).then(data => { 
-        document.querySelector("#mainContainer").innerHTML = "";
-        document.querySelector("#mainContainer").innerHTML = data.about;
+document.querySelector("#gamesBtn").addEventListener("click", ()=>{
 
-    }).catch(error => console.error(error))
 });
 
-document.querySelector("#games").addEventListener("click", ()=>{
-    fetch(path + 'games.json').then(response => response.json()).then(data => { 
-        document.querySelector("#mainContainer").innerHTML = "";
-               
-        for(let i = 1; i<Object.values(data).length - 1; i++){
-            const element = document.createElement('p');
-            element.innerHTML = `<a href="${Object.values(data)[i]}" target="_blank">${Object.keys(data)[i]}</a>`;
-            document.querySelector('#mainContainer').appendChild(element);
-        }
-    }).catch(error => console.error(error))
+document.querySelector("#programsBtn").addEventListener("click", ()=>{
+
 });
 
-document.querySelector("#projects").addEventListener("click", ()=>{
-    fetch(path + 'projects.json').then(response => response.json()).then(data => { 
-        document.querySelector("#mainContainer").innerHTML = "";
-               
-        for(let i = 1; i<Object.values(data).length - 1; i++){
-            const element = document.createElement('p');
-            element.innerHTML = `<a href="${Object.values(data)[i]}" target="_blank">${Object.keys(data)[i]}</a>`;
-            document.querySelector('#mainContainer').appendChild(element);
-        }
-    }).catch(error => console.error(error))
+document.querySelector("#streamBtn").addEventListener("click", ()=>{
+
 });
 
-document.querySelector("#graphic").addEventListener("click", ()=>{
-    fetch(path + 'graphic.json').then(response => response.json()).then(data => { 
-        document.querySelector("#mainContainer").innerHTML = "";
+document.querySelector("#contactBtn").addEventListener("click", ()=>{
 
-        let p = document.createElement("p");
-        p.innerHTML = "Stream z 23 maja 2020 z Neverwinter Nights został usunięty. Ponoć zostały naruszone czyjeś prawa autorskie. Niewierne Psy nie maja pojęcia w jaki sposób to się mogło stać. Życie. W miejsce Neverwiter Nights będzie teraz Total War - Napoleon. Zobaczym jak to wyjdzie.";
-        document.querySelector('#mainContainer').appendChild(p);
-
-        let p2 = document.createElement("p");
-        p2.innerHTML = "Ważna informacja. Od jutra (26 czerwca 2020), do odwołania, będzie zmiana w grafiku oraz w grach. Streamowana będzie także tylko jedna gra: Eve Online. Zmianie ulegnie platforma, na której będą odbywały się streamy, a będzie to twitch.tv. W planach jest nowy grafik. 26 czerwca 2020 będzie przeprowadzany test na wymienionej platformie. Wstępnie streamy będą w piątki (19 - 22) oraz niedzielę (17 - 20). Czasami będzie stream także w soboty (19 - 22).";
-        document.querySelector('#mainContainer').appendChild(p2);
-
-        
-
-    }).catch(error => console.error(error))
 });
 
+
+fetch('./json/test.json').then(response => response.json()).then(data => { 
+
+    console.log(data.test);
+
+}).catch(error => console.error(error))
