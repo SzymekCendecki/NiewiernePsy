67ed460eb8c4ff190e9d28233ccb459f15a7a96f
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2020-08-31 20:10:29 +0200

    Add files via upload

--- js/engine.js
+++ js/engine.js
@@ -87,12 +87,15 @@ var polishTXT = ["Nazwa studia wzięła się od pewnej historii, z próbą wynaj
 
 var englishBtn = ["about us", "games", "programs", "stream"];
 
-// 0 - about, 1 - remastered, 2 - powerfull, 3 - hell, 4 - pirate, 5 - simple, 6-simple2, 7 game Desc, 8 - cinverter, 9 - conveeter 2 , 10 - scss, 11 - atlas, 12 stream, 13 - footer
-
 var englishTXT = ["The name of the studio comes from a story about an attempt to rent a room in Warsaw. In short, the room was not rented to the founder of the studio because ... the atheism of that founder was a problem, and it was problematic for a person who wanted to rent a room (the most authentic story). The date of establishing this unofficial studio is ... Exactly, there is a slight problem with that, as this date has been slightly lost in the darkness of history. For the sake of simplicity, it will be necessary to assume that 'officially' was the same as the creation of the fanpage on Facebook (June 13, 2016). And unofficially, it had to be a little earlier (I guess after the first posts). From the beginning, the studio's plans were to create browser games and learn programming (html, js, css). On January 25, the studio also began streaming games. Three games went first: Eve Online, Neverwinter Nights and World of Tanks. The stream schedule in the 'GRAFIK' tab.", "Audaces Remastered", "Audaces Powerfull", "Hell of the stars", "Pirate cruise", "Simple Game", "Simple Game 2", "Only playable versions of the games are included.", "converter", "converter 2", "SCSS library.", "Political World Atlas '85", "The games are streamed on twitch.tv under the nickname Krupik Hopsas. This channel also has the current stream schedule.", "contact: krupik@autograf.pl"];
 
 var allP = document.querySelectorAll('p');
 var langTxt = document.querySelector("#languageBtn").textContent;
+
+var changeClass = function changeClass(a) {
+    document.querySelector(a).classList.remove("displayNone");
+    document.querySelector(a).classList.add("displayBlock");
+};
 
 switch (langTxt) {
     case 'en':
@@ -129,13 +132,11 @@ document.querySelector("#aboutBtn").addEventListener("click", function () {
 
     for (var _i4 = 0; _i4 < id.length; _i4++) {
         if (document.querySelector("#" + id[_i4]).getAttribute('class') === "displayBlock") {
-            document.querySelector("#" + id[_i4]).classList.remove('displayBlock');
-            document.querySelector("#" + id[_i4]).classList.add('displayNone');
+            document.querySelector("#" + id[_i4]).classList.toggle('displayNone');
         }
     }
 
-    document.querySelector("#about").classList.remove("displayNone");
-    document.querySelector("#about").classList.add("displayBlock");
+    changeClass("#about");
 });
 
 document.querySelector("#gamesBtn").addEventListener("click", function () {
@@ -143,39 +144,33 @@ document.querySelector("#gamesBtn").addEventListener("click", function () {
 
     for (var _i5 = 0; _i5 < id.length; _i5++) {
         if (document.querySelector("#" + id[_i5]).getAttribute('class') === "displayBlock") {
-            document.querySelector("#" + id[_i5]).classList.remove('displayBlock');
-            document.querySelector("#" + id[_i5]).classList.add('displayNone');
+            document.querySelector("#" + id[_i5]).classList.toggle('displayNone');
         }
     }
 
-    document.querySelector("#games").classList.remove("displayNone");
-    document.querySelector("#games").classList.add("displayBlock");
+    changeClass("#games");
 });
 
 document.querySelector("#programsBtn").addEventListener("click", function () {
     for (var _i6 = 0; _i6 < id.length; _i6++) {
 
         if (document.querySelector("#" + id[_i6]).getAttribute('class') === "displayBlock") {
-            document.querySelector("#" + id[_i6]).classList.remove('displayBlock');
-            document.querySelector("#" + id[_i6]).classList.add('displayNone');
+            document.querySelector("#" + id[_i6]).classList.toggle('displayNone');
         }
     }
 
-    document.querySelector("#programs").classList.remove("displayNone");
-    document.querySelector("#programs").classList.add("displayBlock");
+    changeClass("#programs");
 });
 
 document.querySelector("#streamBtn").addEventListener("click", function () {
     for (var _i7 = 0; _i7 < id.length; _i7++) {
 
         if (document.querySelector("#" + id[_i7]).getAttribute('class') === "displayBlock") {
-            document.querySelector("#" + id[_i7]).classList.remove('displayBlock');
-            document.querySelector("#" + id[_i7]).classList.add('displayNone');
+            document.querySelector("#" + id[_i7]).classList.toggle('displayNone');
         }
     }
 
-    document.querySelector("#stream").classList.remove("displayNone");
-    document.querySelector("#stream").classList.add("displayBlock");
+    changeClass("#stream");
 });
 
 document.querySelector("#languageBtn").addEventListener("click", function () {
--- js/workingFile.js
+++ js/workingFile.js
@@ -8,12 +8,15 @@ var polishTXT =["Nazwa studia wzięła się od pewnej historii, z próbą wynaj�
 
 var englishBtn = ["about us", "games", "programs", "stream"];
 
-// 0 - about, 1 - remastered, 2 - powerfull, 3 - hell, 4 - pirate, 5 - simple, 6-simple2, 7 game Desc, 8 - cinverter, 9 - conveeter 2 , 10 - scss, 11 - atlas, 12 stream, 13 - footer
-
 var englishTXT= ["The name of the studio comes from a story about an attempt to rent a room in Warsaw. In short, the room was not rented to the founder of the studio because ... the atheism of that founder was a problem, and it was problematic for a person who wanted to rent a room (the most authentic story). The date of establishing this unofficial studio is ... Exactly, there is a slight problem with that, as this date has been slightly lost in the darkness of history. For the sake of simplicity, it will be necessary to assume that 'officially' was the same as the creation of the fanpage on Facebook (June 13, 2016). And unofficially, it had to be a little earlier (I guess after the first posts). From the beginning, the studio's plans were to create browser games and learn programming (html, js, css). On January 25, the studio also began streaming games. Three games went first: Eve Online, Neverwinter Nights and World of Tanks. The stream schedule in the 'GRAFIK' tab.", "Audaces Remastered", "Audaces Powerfull", "Hell of the stars", "Pirate cruise", "Simple Game", "Simple Game 2", "Only playable versions of the games are included.", "converter", "converter 2", "SCSS library.", "Political World Atlas '85", "The games are streamed on twitch.tv under the nickname Krupik Hopsas. This channel also has the current stream schedule.", "contact: krupik@autograf.pl"];
 
 let allP = document.querySelectorAll('p');
 let langTxt = document.querySelector("#languageBtn").textContent;
+
+let changeClass = (a) => {
+    document.querySelector(a).classList.remove("displayNone");
+    document.querySelector(a).classList.add("displayBlock");
+};
    
 switch (langTxt) {
  case 'en':
@@ -46,20 +49,15 @@ switch (langTxt) {
 
 }
 
-
-
 document.querySelector("#aboutBtn").addEventListener("click", ()=>{   
     
     for(let i = 0; i <id.length; i++){
         if (document.querySelector("#" + id[i]).getAttribute('class') === "displayBlock"){
-            document.querySelector("#" + id[i]).classList.remove('displayBlock');
-            document.querySelector("#" + id[i]).classList.add('displayNone');
+            document.querySelector("#" + id[i]).classList.toggle('displayNone');
         }
     }
    
-    document.querySelector("#about").classList.remove("displayNone");
-    document.querySelector("#about").classList.add("displayBlock");
-         
+    changeClass("#about");        
 });
 
 document.querySelector("#gamesBtn").addEventListener("click", ()=>{
@@ -67,55 +65,43 @@ document.querySelector("#gamesBtn").addEventListener("click", ()=>{
 
     for(let i = 0; i <id.length; i++){
         if (document.querySelector("#" + id[i]).getAttribute('class') === "displayBlock"){
-            document.querySelector("#" + id[i]).classList.remove('displayBlock');
-            document.querySelector("#" + id[i]).classList.add('displayNone');
+            document.querySelector("#" + id[i]).classList.toggle('displayNone');
         }
     }
    
-    document.querySelector("#games").classList.remove("displayNone");
-    document.querySelector("#games").classList.add("displayBlock");
-
+    changeClass("#games");
 });
 
 document.querySelector("#programsBtn").addEventListener("click", ()=>{
     for(let i = 0; i <id.length; i++){
 
         if (document.querySelector("#" + id[i]).getAttribute('class') === "displayBlock"){
-            document.querySelector("#" + id[i]).classList.remove('displayBlock');
-            document.querySelector("#" + id[i]).classList.add('displayNone');
+            document.querySelector("#" + id[i]).classList.toggle('displayNone');
         }
      
     }
    
-    document.querySelector("#programs").classList.remove("displayNone");
-    document.querySelector("#programs").classList.add("displayBlock");
-
-
+    changeClass("#programs");
 });
 
 document.querySelector("#streamBtn").addEventListener("click", ()=>{
     for(let i = 0; i <id.length; i++){
 
         if (document.querySelector("#" + id[i]).getAttribute('class') === "displayBlock"){
-            document.querySelector("#" + id[i]).classList.remove('displayBlock');
-            document.querySelector("#" + id[i]).classList.add('displayNone');
+            document.querySelector("#" + id[i]).classList.toggle('displayNone');
         }
      
     }
    
-    document.querySelector("#stream").classList.remove("displayNone");
-    document.querySelector("#stream").classList.add("displayBlock");
+    changeClass("#stream");
 });
-
-
 
 
 document.querySelector("#languageBtn").addEventListener("click", ()=>{
 
     let langTxt = document.querySelector("#languageBtn").textContent;
-
     
-   switch (langTxt) {
+    switch (langTxt) {
     case 'en':
    
         document.querySelector("#languageBtn").innerHTML = "pl";
